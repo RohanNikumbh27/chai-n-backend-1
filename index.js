058c3f5c6765e3ab705e143ef457d55ed31ebcde
Rohan Nikumbh <rohannikumbh2793@gmail.com>
2025-11-26 00:05:06 +0530

    cors

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
     message: "Welcome to the Chai-n Backend!",
   });
 });
+
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "Why don't skeletons fight each other? They don't have the guts.",
+  ];
+  res.json({data: jokes});
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
